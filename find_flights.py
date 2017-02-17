b31be9cc711a1217622d7cbeefd172326120c7c0
Ekaterina <ivanovakate43@gmail.com>
2017-02-17 11:24:46 +0300

    START

---
+++ find_flights.py
@@ -0,0 +1,17 @@
+import argparse
+from library_find import Finding
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-iIATA', type=str, help='')
+    parser.add_argument('-oIATA', type=str, help='')
+    parser.add_argument('-idata', type=str, help='')
+    parser.add_argument('-odata', type=str, help='')
+
+    args = parser.parse_args()
+    f = Finding(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ library_find.py
@@ -0,0 +1,6 @@
+class Finding(object):
+    def __init__(self, args):
+        self.iiata = args.iIATA
+        self.oiata = args.oIATA
+        self.idata = args.idata
+        self.odata = args.odata
