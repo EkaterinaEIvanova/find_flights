988af134e8fcc6514cfba83f3fc042f535d8cf65
Ekaterina <ivanovakate43@gmail.com>
2017-02-19 15:48:45 +0300

    Исправлено получение данных

--- find_flights.py
+++ find_flights.py
@@ -1,20 +1,35 @@
 import argparse
-from iata_codes import IATACodesClient
 
 from library_find import Finding
+from iata_codes.cities import IATACodesClient
 
+def inspect_data(args):
+    client = IATACodesClient('15be9538-c84a-465d-b82d-6d668e7f1b4e')
+    iiata = client.get(code=args.iIATA)
+    oiata = client.get(code=args.oIATA)
+
+    if iiata and oiata :
+        args.i_name = iiata[0][u'name']
+        args.o_name = oiata[0][u'name']
 
 def main():
-    parser = argparse.ArgumentParser()
+    '''  parser = argparse.ArgumentParser()
     parser.add_argument('-iIATA', type=str, help='')
     parser.add_argument('-oIATA', type=str, help='')
     parser.add_argument('-idata', type=str, help='')
     parser.add_argument('-odata', type=str, help='')
 
-    args = parser.parse_args()
-    client = IATACodesClient('BER')
-    print(client.get(name='Moscow'))
-   # f = Finding(args)
+    args = parser.parse_args()'''
+    args = argparse.Namespace()
+    args.iIATA = 'LON'
+    args.oIATA = 'BER'
+    args.idata = '2017-02-22'
+    args.odata = '2017-02-22'
+    inspect_data(args)
+    f = Finding(args)
+    f.get_rbody()
+    f.get_flights()
+   # f.get_flights()
 
 if __name__ == "__main__":
     main()
--- library_find.py
+++ library_find.py
@@ -1,53 +1,48 @@
-from lxml import etree
-from lxml import html
-import json
 import requests
-from six import BytesIO
-from six import StringIO
+from lxml import html
 
 
 class Finding(object):
     def __init__(self, args):
-        self.iiata = args.iIATA
-        self.oiata = args.oIATA
+        self.i_iata = args.iIATA
+        self.o_iata = args.oIATA
+        self.i_name = args.i_name
+        self.o_name = args.o_name
         self.idata = args.idata
         self.odata = args.odata
-        self.r = self.get_rbody()
-        self.get_flights()
+        self.resp_body  = ''
+        self.flights = []
 
-    def get_rbody(self):
+    def get_body(self):
         headers = {'Content-Type': 'application/x-www-form-urlencoded',
-                   'Cookie':'startConnection=DME@MUC@2017-03-31@2017-03-31; ABSESS=kbfj0b77ueq4sh5s9gpg8np392;'
+                   'Cookie':'startConnection={}@{}@{}@{}1; ABSESS=kbfj0b77ueq4sh5s9gpg8np392;'.
+                       format(self.i_iata, self.o_iata, self.idata, self.odata)
                    }
 
         url = 'http://www.flyniki.com/en/booking/flight/vacancy.php?sid=ba9d2bc0faf73daf9c69'
-        data = {'_ajax[templates][]': ['main','priceoverview', 'infos','flightinfo'],
-                '_ajax[requestParams][departure]':'Moscow - Domodedovo',
-                '_ajax[requestParams][destination]':'Munich',
-                '_ajax[requestParams][returnDeparture]':'',
-                '_ajax[requestParams][returnDestination]':'',
-                '_ajax[requestParams][outboundDate]': '2017-03-31',
-                '_ajax[requestParams][returnDate]':'2017-03-31',
-                '_ajax[requestParams][adultCount]':'1',
-                '_ajax[requestParams][childCount]':'0',
-                '_ajax[requestParams][infantCount]':'0',
-                '_ajax[requestParams][openDateOverview]':'',
-                '_ajax[requestParams][oneway]':'',
+        data = {'_ajax[templates][]': ['main', 'priceoverview', 'infos', 'flightinfo'],
+                '_ajax[requestParams][departure]': '{}'.format(self.i_name),
+                '_ajax[requestParams][destination]': '{}'.format(self.o_name),
+                '_ajax[requestParams][returnDeparture]': '',
+                '_ajax[requestParams][returnDestination]': '',
+                '_ajax[requestParams][outboundDate]': '{}'.format(self.idata),
+                '_ajax[requestParams][returnDate]': '{}'.format(self.odata),
+                '_ajax[requestParams][adultCount]': '1',
+                '_ajax[requestParams][childCount]': '0',
+                '_ajax[requestParams][infantCount]': '0',
+                '_ajax[requestParams][openDateOverview]': '',
+                '_ajax[requestParams][oneway]': '',
                 }
-        return requests.post(url, headers=headers, data=data).json()['templates']['main']
+        self.resp_body = requests.post(url, headers=headers, data=data).json()['templates']['main']
 
     def get_flights(self):
-        tree = html.fromstring(self.r)
-        outbound_flight = tree.xpath('// *[@id="flighttables"]/div[1]/div[1]/div[1]/div/div/text()')[0]
-        return_flight = tree.xpath('//*[@id="flighttables"]/div[3]/div[1]/div[1]/div/div/text()')[0]
-        cur = tree.xpath('//*[@id="flight-table-header-price-ECO_COMF"]/text()')[0]
-        if not cur:
-            cur = tree.xpath('//*[@id="flight-table-header-price-ECO_PREM"]/text()')[0]
-        time = tree.xpath('.//table[@class="flighttable"]/tbody/tr/td[2]/span/*/text()')
-        stop =  tree.xpath('.//table[@class="flighttable"]/tbody/tr/td[3]/text()')
-        duration = tree.xpath('.//table[@class="flighttable"]/tbody/tr/td[4]/span/text()')
-        eco_price = tree.xpath('.//table[@class="flighttable"]/tbody/tr/td[5]/label/div[2]/span/text()')
-        comf_price = tree.xpath('.//table[@class="flighttable"]/tbody/tr/td[6]/label/div[1]/span/text()')
+        tree = html.fromstring(self.resp_body)
+        outbound_data = tree.xpath('// *[@id="flighttables"]/div[1]/div[1]/div[1]/div/div/text()')[0]
+        return_data = tree.xpath('//*[@id="flighttables"]/div[3]/div[1]/div[1]/div/div/text()')[0]
+        currency = tree.xpath('//*[@id="flight-table-header-price-ECO_COMF"]/text()')[0]
 
-
-
+        for tb in tree.xpath('.//table[@class="flighttable"]'):
+            flights = []
+            for tr in tb.xpath('./tbody/tr[position() mod 2 = 1]'):
+                flights.append(tr.xpath('./td//text()[string-length()>1]'))
+            self.flights.append(flights)
