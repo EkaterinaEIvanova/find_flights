7487f1240da4eed8216cee28d8a6de2b2eb34a77
Ekaterina <ivanovakate43@gmail.com>
2017-02-20 21:45:23 +0300

    Обработка ошибок при вводе с CMD

--- find_flights.py
+++ find_flights.py
@@ -1,30 +1,46 @@
+# -*- coding: utf-8 -*-
 import argparse
-
-from library_find import Finding
+from datetime import datetime
 from iata_codes.cities import IATACodesClient
 
-def inspect_data(args):
-    client = IATACodesClient('15be9538-c84a-465d-b82d-6d668e7f1b4e')
-    iiata = client.get(code=args.iIATA)
-    oiata = client.get(code=args.oIATA)
+from library_find import Finding
 
-    if iiata and oiata:
-        args.i_name = iiata[0][u'name']
-        args.o_name = oiata[0][u'name']
+
+def inspect_data(args):
+    """
+    Проверяю правильность введенных IATA кодов, и дат. Если IATA верны сопоставляю им полное название аэропортов.
+    :param args:
+    :return: True/False
+    """
+    client = IATACodesClient('15be9538-c84a-465d-b82d-6d668e7f1b4e')
+    try:
+        args.source_name = client.get(code=args.sourceIATA)[0][u'name']
+        args.destin_name = client.get(code=args.destinationIATA)[0][u'name']
+    except LookupError:
+        print ('IATA-code is not correct. Enter correct code.')
+        return False
+    try:
+        datetime.strptime(args.outbound_date, '%Y-%m-%d')
+        if args.return_date:
+            datetime.strptime(args.return_date, '%Y-%m-%d')
+    except ValueError:
+        print ('Incorrect date format. Please, enter the date in the format Y-M-D')
+        return False
+    return True
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-iIATA', type=str, help='')
-    parser.add_argument('-oIATA', type=str, help='')
-    parser.add_argument('-idata', type=str, help='')
-    parser.add_argument('-odata', type=str, help='')
-
+    parser.add_argument('-sIATA', '--sourceIATA', type=str, required=True, help='IATA source airport code')
+    parser.add_argument('-dIATA', '--destinationIATA', type=str, required=True, help='IATA destination airport code')
+    parser.add_argument('-odate', '--outbound_date', type=str, required=True, help='Outbound date, format Y-M-D',)
+    parser.add_argument('-rdate', '--return_date', type=str, help='Return date, format Y-M-D')
     args = parser.parse_args()
 
-    inspect_data(args)
-    f = Finding(args)
-    f.get_flights()
-    print(f)
+    if inspect_data(args):
+        f = Finding(args)
+        f.get_flights()
+        print(f)
 
 if __name__ == "__main__":
     main()
--- library_find.py
+++ library_find.py
@@ -5,12 +5,12 @@ from lxml import html
 
 class Finding(object):
     def __init__(self, args):
-        self.i_iata = args.iIATA
-        self.o_iata = args.oIATA
-        self.i_name = args.i_name
-        self.o_name = args.o_name
-        self.idata = args.idata
-        self.odata = args.odata
+        self.s_iata = args.sourceIATA
+        self.d_iata = args.destinationIATA
+        self.s_name = args.source_name
+        self.d_name = args.destin_name
+        self.r_date = args.return_date
+        self.o_date = args.outbound_date
         self.content = self._get_content()
         self.outbound_data = ''
         self.return_data = ''
@@ -19,23 +19,26 @@ class Finding(object):
         self.flights_str = []
 
     def _get_content(self):
-        '''
+        """
         Делаю http-запрос к сайту, получаю ответ и если есть, то забираю из него контент.
         :return:content
-        '''
+        """
         headers = {'Content-Type': 'application/x-www-form-urlencoded',
-                   'Cookie':'startConnection={}@{}@{}@{}; ABSESS=kbfj0b77ueq4sh5s9gpg8np392;'.
-                       format(self.i_iata, self.o_iata, self.idata, self.odata)
+                   'Cookie': 'startConnection={}@{}@{}@{}; ABSESS=kbfj0b77ueq4sh5s9gpg8np392;'.format(
+                       self.s_iata,
+                       self.d_iata,
+                       self.o_date,
+                       self.r_date)
                    }
 
         url = 'http://www.flyniki.com/en/booking/flight/vacancy.php?sid=ba9d2bc0faf73daf9c69'
         data = {'_ajax[templates][]': ['main', 'priceoverview', 'infos', 'flightinfo'],
-                '_ajax[requestParams][departure]': '{}'.format(self.i_name),
-                '_ajax[requestParams][destination]': '{}'.format(self.o_name),
+                '_ajax[requestParams][departure]': '{}'.format(self.s_name),
+                '_ajax[requestParams][destination]': '{}'.format(self.d_name),
                 '_ajax[requestParams][returnDeparture]': '',
                 '_ajax[requestParams][returnDestination]': '',
-                '_ajax[requestParams][outboundDate]': '{}'.format(self.idata),
-                '_ajax[requestParams][returnDate]': '{}'.format(self.odata),
+                '_ajax[requestParams][outboundDate]': '{}'.format(self.o_date),
+                '_ajax[requestParams][returnDate]': '{}'.format(self.r_date),
                 '_ajax[requestParams][adultCount]': '1',
                 '_ajax[requestParams][childCount]': '0',
                 '_ajax[requestParams][infantCount]': '0',
@@ -49,10 +52,10 @@ class Finding(object):
             return None
 
     def get_flights(self):
-        '''
-           Забираю из контента только нужные данные по рейсам.
+        """
+        Забираю из контента только нужные данные по рейсам.
         :return:
-        '''
+        """
         if self.content:
             tree = html.fromstring(self.content)
             self.outbound_data = tree.xpath('// *[@id="flighttables"]/div[1]/div[1]/div[1]/div/div/text()')[0]
@@ -69,16 +72,16 @@ class Finding(object):
 
     def __str__(self):
         if self.flights:
-            for i,flights in enumerate(self.flights):
+            for i, flights in enumerate(self.flights):
                 self.flights[i].sort(key=lambda j: j[-1])
                 self.flights_str.append('\n'.join([' '.join(fl) for fl in flights]))
             head = u'start end      duration    {},econ {},comf'.format(self.currency.rstrip(),
-                                                                            self.currency.rstrip(),
-                                                                            self.currency.rstrip())
+                                                                        self.currency.rstrip(),
+                                                                        self.currency.rstrip())
 
             return '{}\n{}\n{}\n\n{}\n{}\n{}'.format(self.outbound_data.encode('utf-8').lstrip(),
-                                                 head.encode('utf-8'),
-                                                 self.flights_str[0].encode('utf-8'),
-                                                 self.return_data.encode('utf-8').lstrip(),
-                                                 head.encode('utf-8'),
-                                                 self.flights_str[1].encode('utf-8'))
+                                                     head.encode('utf-8'),
+                                                     self.flights_str[0].encode('utf-8'),
+                                                     self.return_data.encode('utf-8').lstrip(),
+                                                     head.encode('utf-8'),
+                                                     self.flights_str[1].encode('utf-8'))
